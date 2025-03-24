7166f8c6809aa59344fff2ff441004b209ea3fd6
Xayah <ycxiang14sep@gmail.com>
2025-03-25 00:27:08 +0900

    update

--- houdini_executor.py
+++ houdini_executor.py
@@ -7,7 +7,7 @@ import math
 import tqdm
 
 
-class DensityOnlyExecutor:
+class HoudiniExecutor:
     def __init__(self, batch_size: int, depth_size: int, ratio: float, target_device: torch.device, target_dtype: torch.dtype):
         videos_data = load_videos_data(*training_videos, ratio=ratio, dtype=target_dtype)
         poses, focals, width, height, near, far = load_cameras_data(*camera_calibrations, ratio=ratio, device=target_device, dtype=target_dtype)
@@ -28,7 +28,8 @@ class DensityOnlyExecutor:
         self.optimizer_v = optimizer_v
         target_lr_ratio = 0.1
         gamma = math.exp(math.log(target_lr_ratio) / 30000)
-        self.scheduler = torch.optim.lr_scheduler.ExponentialLR(self.optimizer_d, gamma=gamma)
+        self.scheduler_d = torch.optim.lr_scheduler.ExponentialLR(self.optimizer_d, gamma=gamma)
+        self.scheduler_v = torch.optim.lr_scheduler.ExponentialLR(self.optimizer_v, gamma=gamma)
 
         self.videos_data = videos_data
         self.poses, self.focals, self.width, self.height, self.near, self.far = poses, focals, width, height, near, far
@@ -62,7 +63,7 @@ class DensityOnlyExecutor:
         self.videos_data_resampled = resample_frames(frames=self.videos_data, u=u, v=v).to(self.target_device)  # (T, V, H, W, C)
         self.generator = sample_frustum(dirs=dirs, poses=self.poses, batch_size=self.batch_size, randomize=True, device=self.target_device)
 
-    def optimize_iter(self):
+    def optimize_density(self):
         self.optimizer_d.zero_grad()
         try:
             batch_indices, batch_rays_o, batch_rays_d = next(self.generator)
@@ -72,9 +73,27 @@ class DensityOnlyExecutor:
         img_loss = self.density_only_loss(batch_indices, batch_rays_o, batch_rays_d)
         img_loss.backward()
         self.optimizer_d.step()
-        self.scheduler.step()
+        self.scheduler_d.step()
         self.global_step += 1
-        tqdm.tqdm.write(f"iter: {self.global_step}, lr: {self.scheduler.get_last_lr()[0]}, img_loss: {img_loss}")
+        tqdm.tqdm.write(f"iter: {self.global_step}, lr_d: {self.scheduler_d.get_last_lr()[0]}, img_loss: {img_loss}")
+
+    def optimize_joint(self):
+        self.optimizer_d.zero_grad()
+        self.optimizer_v.zero_grad()
+        try:
+            batch_indices, batch_rays_o, batch_rays_d = next(self.generator)
+        except StopIteration:
+            self.refresh_generator()
+            batch_indices, batch_rays_o, batch_rays_d = next(self.generator)
+        nseloss_fine, img_loss, proj_loss, min_vel_reg = self.velocity_loss(batch_indices, batch_rays_o, batch_rays_d)
+        vel_loss = nseloss_fine + 10000 * img_loss + 1.0 * proj_loss + 10.0 * min_vel_reg
+        vel_loss.backward()
+        self.optimizer_d.step()
+        self.optimizer_v.step()
+        self.scheduler_d.step()
+        self.scheduler_v.step()
+        self.global_step += 1
+        tqdm.tqdm.write(f"iter: {self.global_step}, lr_d: {self.scheduler_d.get_last_lr()[0]}, lr_v: {self.scheduler_v.get_last_lr()[0]}, nseloss_fine: {nseloss_fine}, img_loss: {img_loss}, proj_loss: {proj_loss}, min_vel_reg: {min_vel_reg}")
 
     @torch.compile
     def density_only_loss(self, batch_indices, batch_rays_o, batch_rays_d):
@@ -119,129 +138,11 @@ class DensityOnlyExecutor:
 
         return img_loss
 
-    def save_ckpt(self, directory: str):
-        from datetime import datetime
-        timestamp = datetime.now().strftime('%m%d%H')
-        filename = 'ckpt_{}_bs{}_{:06d}.tar'.format(timestamp, self.batch_size, self.global_step)
-        os.makedirs(directory, exist_ok=True)
-        path = os.path.join(directory, filename)
-        torch.save({
-            'global_step': self.global_step,
-            'model_d': self.model_d.state_dict(),
-            'encoder_d': self.encoder_d.state_dict(),
-            'optimizer_d': self.optimizer_d.state_dict(),
-            'model_v': self.model_v.state_dict(),
-            'encoder_v': self.encoder_v.state_dict(),
-            'optimizer_v': self.optimizer_v.state_dict(),
-        }, path)
-
-    def load_ckpt(self, path: str):
-        checkpoint = torch.load(path)
-        self.global_step = checkpoint['global_step']
-        self.model_d.load_state_dict(checkpoint['model_d'])
-        self.encoder_d.load_state_dict(checkpoint['encoder_d'])
-        self.optimizer_d.load_state_dict(checkpoint['optimizer_d'])
-        self.model_v.load_state_dict(checkpoint['model_v'])
-        self.encoder_v.load_state_dict(checkpoint['encoder_v'])
-        self.optimizer_v.load_state_dict(checkpoint['optimizer_v'])
-        tqdm.tqdm.write(f"loaded checkpoint from {path}")
-
-    @torch.compile
-    def sample_density_grid(self, resx, resy, resz, frame):
-        with torch.no_grad():
-            xs, ys, zs = torch.meshgrid([torch.linspace(0, 1, resx, device=self.target_device), torch.linspace(0, 1, resy, device=self.target_device), torch.linspace(0, 1, resz, device=self.target_device)], indexing='ij')
-            coord_3d_sim = torch.stack([xs, ys, zs], dim=-1)
-            coord_3d_world = sim2world(coord_3d_sim, self.s2w, self.s_scale)
-            input_xyzt_flat = torch.cat([coord_3d_world, torch.ones_like(coord_3d_world[..., :1]) * float(frame / 120.0)], dim=-1).reshape(-1, 4)
-            raw_d_flat_list = []
-            batch_size = 64 * 64 * 64
-            for i in range(0, input_xyzt_flat.shape[0], batch_size):
-                input_xyzt_flat_batch = input_xyzt_flat[i:i + batch_size]
-                raw_d_flat_batch = self.model_d(self.encoder_d(input_xyzt_flat_batch))
-                raw_d_flat_list.append(raw_d_flat_batch)
-            raw_d_flat = torch.cat(raw_d_flat_list, dim=0)
-            raw_d = raw_d_flat.reshape(resx, resy, resz, 1)
-            return raw_d.to(torch.device('cpu'))
-
-    @torch.compile
-    def sample_points(self, resx, resy, resz):
-        with torch.no_grad():
-            xs, ys, zs = torch.meshgrid([torch.linspace(0, 1, resx, device=self.target_device), torch.linspace(0, 1, resy, device=self.target_device), torch.linspace(0, 1, resz, device=self.target_device)], indexing='ij')
-            coord_3d_sim = torch.stack([xs, ys, zs], dim=-1)
-            coord_3d_world = sim2world(coord_3d_sim, self.s2w, self.s_scale)
-            return coord_3d_world.to(torch.device('cpu'))
-
-
-class HoudiniExecutor:
-    def __init__(self, batch_size: int, depth_size: int, ratio: float, target_device: torch.device, target_dtype: torch.dtype):
-        videos_data = load_videos_data(*training_videos, ratio=ratio, dtype=target_dtype)
-        poses, focals, width, height, near, far = load_cameras_data(*camera_calibrations, ratio=ratio, device=target_device, dtype=target_dtype)
-        encoder_d = HashEncoderNativeFasterBackward().to(target_device)
-        model_d = NeRFSmall(num_layers=2, hidden_dim=64, geo_feat_dim=15, num_layers_color=2, hidden_dim_color=16, input_ch=encoder_d.num_levels * 2).to(target_device)
-        optimizer_d = torch.optim.RAdam([{'params': model_d.parameters(), 'weight_decay': 1e-6}, {'params': encoder_d.parameters(), 'eps': 1e-15}], lr=0.001, betas=(0.9, 0.99))
-        encoder_v = HashEncoderNativeFasterBackward().to(target_device)
-        model_v = NeRFSmallPotential(num_layers=2, hidden_dim=64, geo_feat_dim=15, num_layers_color=2, hidden_dim_color=16, input_ch=encoder_v.num_levels * 2, use_f=False).to(target_device)
-        optimizer_v = torch.optim.RAdam([{'params': model_v.parameters(), 'weight_decay': 1e-6}, {'params': encoder_v.parameters(), 'eps': 1e-15}], lr=0.001, betas=(0.9, 0.99))
-
-        # =============================================================================
-        # TO DELETE
-        VOXEL_TRAN = torch.tensor([
-            [1.0, 0.0, 7.5497901e-08, 8.1816666e-02],
-            [0.0, 1.0, 0.0, -4.4627272e-02],
-            [7.5497901e-08, 0.0, -1.0, -4.9089999e-03],
-            [0.0, 0.0, 0.0, 1.0]
-        ], device=target_device, dtype=target_dtype)
-
-        VOXEL_SCALE = torch.tensor([0.4909, 0.73635, 0.4909], device=target_device, dtype=target_dtype)
-
-        NEAR_float, FAR_float = float(near[0].item()), float(far[0].item())
-
-        self.s_w2s = torch.inverse(VOXEL_TRAN).expand([4, 4])
-        self.s2w = torch.inverse(self.s_w2s)
-        self.s_scale = VOXEL_SCALE.expand([3])
-        self.s_min = torch.tensor([0.15, 0.0, 0.15], device=target_device, dtype=target_dtype)
-        self.s_max = torch.tensor([0.85, 1.0, 0.85], device=target_device, dtype=target_dtype)
-
-        # TO DELETE
-        # =============================================================================
-
-        dirs, u, v = shuffle_uv(focals=focals, width=int(width[0].item()), height=int(height[0].item()), randomize=True, device=torch.device("cpu"), dtype=target_dtype)
-        videos_data_resampled = resample_frames(frames=videos_data, u=u, v=v).to(target_device)  # (T, V, H, W, C)
-        dirs = dirs.to(target_device)
-
-        self.generator = sample_frustum(dirs=dirs, poses=poses, batch_size=batch_size, randomize=True, device=target_device)
-        self.encoder_d = encoder_d
-        self.model_d = model_d
-        self.optimizer_d = optimizer_d
-        self.encoder_v = encoder_v
-        self.model_v = model_v
-        self.optimizer_v = optimizer_v
-        self.videos_data_resampled = videos_data_resampled
-        self.target_device = target_device
-        self.target_dtype = target_dtype
-
-        self.NEAR_float = NEAR_float
-        self.FAR_float = FAR_float
-        self.batch_size = batch_size
-        self.depth_size = depth_size
-
-        self.global_step = 1
-        self.lrate = 0.01
-        self.lrate_decay = 1000
-
-    def forward_1_iter(self):
+    def velocity_loss(self, batch_indices, batch_rays_o, batch_rays_d):
         def g(x):
             return self.model_d(x)
 
-        self.optimizer_d.zero_grad()
-        self.optimizer_v.zero_grad()
-
-        try:
-            batch_indices, batch_rays_o, batch_rays_d = next(self.generator)
-        except StopIteration:
-            return False
         batch_time, batch_target_pixels = sample_random_frame(videos_data=self.videos_data_resampled, batch_indices=batch_indices, device=self.target_device, dtype=self.target_dtype)
-
         batch_size_current = batch_rays_d.shape[0]
 
         t_vals = torch.linspace(0., 1., steps=self.depth_size, device=self.target_device, dtype=self.target_dtype)
@@ -306,26 +207,7 @@ class HoudiniExecutor:
         if nse_errors.sum() > 10000:
             print(f'skip large loss {nse_errors.sum():.3g}, timestep={batch_points_time_flat_filtered[0, 3]}')
             return True
-
-        vel_loss = nseloss_fine + 10000 * img_loss + 1.0 * proj_loss + 10.0 * min_vel_reg
-        vel_loss.backward()
-
-        self.optimizer_d.step()
-        self.optimizer_v.step()
-
-        decay_rate = 0.1
-        decay_steps = self.lrate_decay
-        new_lrate = self.lrate * (decay_rate ** (self.global_step / decay_steps))
-        for param_group in self.optimizer_d.param_groups:
-            param_group['lr'] = new_lrate
-        for param_group in self.optimizer_v.param_groups:
-            param_group['lr'] = new_lrate
-
-        self.global_step += 1
-
-        print(f"iter: {self.global_step}, new_lrate: {new_lrate}, nseloss_fine: {nseloss_fine}, img_loss: {10000 * img_loss}, proj_loss: {proj_loss}, min_vel_reg: {10.0 * min_vel_reg}, vel_loss: {vel_loss}")
-
-        return True
+        return nseloss_fine, img_loss, proj_loss, min_vel_reg
 
     def save_ckpt(self, directory: str):
         from datetime import datetime
@@ -352,7 +234,7 @@ class HoudiniExecutor:
         self.model_v.load_state_dict(checkpoint['model_v'])
         self.encoder_v.load_state_dict(checkpoint['encoder_v'])
         self.optimizer_v.load_state_dict(checkpoint['optimizer_v'])
-        print(f"loaded checkpoint from {path}")
+        tqdm.tqdm.write(f"loaded checkpoint from {path}")
 
     @torch.compile
     def sample_density_grid(self, resx, resy, resz, frame):
@@ -388,29 +270,19 @@ class HoudiniExecutor:
             raw_vel = raw_vel_flat.reshape(resx, resy, resz, 3)
             return raw_vel.to(torch.device('cpu'))
 
+    @torch.compile
     def sample_points(self, resx, resy, resz):
-        xs, ys, zs = torch.meshgrid([torch.linspace(0, 1, resx, device=self.target_device), torch.linspace(0, 1, resy, device=self.target_device), torch.linspace(0, 1, resz, device=self.target_device)], indexing='ij')
-        coord_3d_sim = torch.stack([xs, ys, zs], dim=-1)
-        coord_3d_world = sim2world(coord_3d_sim, self.s2w, self.s_scale)
-        return coord_3d_world.to(torch.device('cpu'))
+        with torch.no_grad():
+            xs, ys, zs = torch.meshgrid([torch.linspace(0, 1, resx, device=self.target_device), torch.linspace(0, 1, resy, device=self.target_device), torch.linspace(0, 1, resz, device=self.target_device)], indexing='ij')
+            coord_3d_sim = torch.stack([xs, ys, zs], dim=-1)
+            coord_3d_world = sim2world(coord_3d_sim, self.s2w, self.s_scale)
+            return coord_3d_world.to(torch.device('cpu'))
 
 
-
-
-def run_train():
+def run_sample_grids(ckpt_path):
     torch.set_float32_matmul_precision('high')
     executor = HoudiniExecutor(batch_size=1024, depth_size=192, ratio=0.5, target_device=torch.device("cuda:0"), target_dtype=torch.float32)
-    pbar = tqdm.tqdm(desc="Running forward_1_iter", unit="iter")
-    while executor.forward_1_iter():
-        pbar.update(1)
-    pbar.close()
-    executor.save_ckpt('houdini/ckpt')
-
-
-def run_sample_grids():
-    torch.set_float32_matmul_precision('high')
-    executor = HoudiniExecutor(batch_size=1024, depth_size=192, ratio=0.5, target_device=torch.device("cuda:0"), target_dtype=torch.float32)
-    executor.load_ckpt('houdini/ckpt/den_002533.tar')
+    executor.load_ckpt(ckpt_path)
     raw_d = executor.sample_density_grid(64, 64, 64, 0)
     raw_vel = executor.sample_velocity_grid(64, 64, 64, 0)
     print(raw_d.shape, raw_vel.shape)
@@ -418,15 +290,33 @@ def run_sample_grids():
 
 def run_train_density_only():
     torch.set_float32_matmul_precision('high')
-    executor = DensityOnlyExecutor(batch_size=1024, depth_size=192, ratio=0.5, target_device=torch.device("cuda:0"), target_dtype=torch.float32)
-    pbar = tqdm.tqdm(desc="Running forward_1_iter", unit="iter")
-    for _ in range(1000):
-        executor.optimize_iter()
-        pbar.update(1)
-    executor.save_ckpt('houdini/ckpt_den_only')
+    executor = HoudiniExecutor(batch_size=1024, depth_size=192, ratio=0.5, target_device=torch.device("cuda:0"), target_dtype=torch.float32)
+    pbar = tqdm.tqdm(desc="run_train_density_only", unit="iter")
+    try:
+        for _ in range(1000):
+            executor.optimize_density()
+            pbar.update(1)
+    except Exception as e:
+        print(e)
+    finally:
+        executor.save_ckpt('houdini/ckpt_den_only')
+
+
+def run_train_joint():
+    torch.set_float32_matmul_precision('high')
+    executor = HoudiniExecutor(batch_size=1024, depth_size=192, ratio=0.5, target_device=torch.device("cuda:0"), target_dtype=torch.float32)
+    pbar = tqdm.tqdm(desc="run_train_joint", unit="iter")
+    try:
+        for _ in range(1000):
+            executor.optimize_joint()
+            pbar.update(1)
+    except Exception as e:
+        print(e)
+    finally:
+        executor.save_ckpt('houdini/ckpt_joint')
 
 
 if __name__ == '__main__':
-    # run_train()
-    # run_sample_grids()
-    run_train_density_only()
+    # run_train_density_only()
+    run_train_joint()
+    # run_sample_grids('houdini/ckpt/den_002533.tar')
