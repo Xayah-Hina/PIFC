142f0ed9343fea8830015577c94024ca760b8f75
Xayah <ycxiang14sep@gmail.com>
2025-03-31 23:02:03 +0900

    train_velocity_only

--- app.py
+++ app.py
@@ -18,6 +18,26 @@ def refresh_generator(batch_size, videos_data, poses, focals, width, height, tar
     return generator, videos_data_resampled
 
 
+def sample_bbox(resx, resy, resz, batch_size, randomize, target_device, target_dtype, s2w, s_w2s, s_scale, s_min, s_max):
+    xs, ys, zs = torch.meshgrid([torch.linspace(0, 1, resx, device=target_device, dtype=target_dtype), torch.linspace(0, 1, resy, device=target_device, dtype=target_dtype), torch.linspace(0, 1, resz, device=target_device, dtype=target_dtype)], indexing='ij')
+    coord_3d_sim = torch.stack([xs, ys, zs], dim=-1)
+    coord_3d_world = sim2world(coord_3d_sim, s2w, s_scale)
+    bbox_mask = insideMask(coord_3d_world, s_w2s, s_scale, s_min, s_max, to_float=False)
+    coord_3d_world_filtered = coord_3d_world[bbox_mask].reshape(-1, 3)
+
+    num_points = coord_3d_world_filtered.shape[0]
+
+    if randomize:
+        indices = torch.randperm(num_points, device=target_device)
+    else:
+        indices = torch.arange(num_points, device=target_device)
+
+    for i in range(0, num_points, batch_size):
+        batch_indices = indices[i:i + batch_size]
+        batch_points = coord_3d_world_filtered[batch_indices]
+        yield batch_points
+
+
 class TrainModel:
     def __init__(self, batch_size: int, ratio: float, target_device: torch.device, target_dtype: torch.dtype):
         self._load_model(target_device)
@@ -75,6 +95,18 @@ class TrainModel:
         self.global_step += 1
         tqdm.tqdm.write(f"iter: {self.global_step}, lr_d: {self.scheduler_d.get_last_lr()[0]}, img_loss: {img_loss}")
         return img_loss
+
+    def optimize_velocity(self, batch_points):
+        self.optimizer_v.zero_grad()
+        skip, nseloss_fine, proj_loss, min_vel_reg = self.velocity_loss(batch_points)
+        if not skip:
+            vel_loss = 1.0 * nseloss_fine + 1.0 * proj_loss + 10.0 * min_vel_reg
+            vel_loss.backward()
+            self.optimizer_v.step()
+            self.scheduler_v.step()
+            self.global_step += 1
+            tqdm.tqdm.write(f"iter: {self.global_step}, lr_d: {self.scheduler_d.get_last_lr()[0]}, lr_v: {self.scheduler_v.get_last_lr()[0]}, nseloss_fine: {nseloss_fine}, proj_loss: {proj_loss}, min_vel_reg: {min_vel_reg}")
+        return nseloss_fine, proj_loss, min_vel_reg
 
     def optimize_joint(self, depth_size: int):
         self.optimizer_d.zero_grad()
@@ -138,6 +170,50 @@ class TrainModel:
         img_loss = torch.nn.functional.mse_loss(rgb_map, batch_target_pixels)
 
         return img_loss
+
+    def velocity_loss(self, batch_points):
+        def g(x):
+            return self.model_d(x)
+
+        batch_time = torch.rand((), device=self.target_device, dtype=self.target_dtype)
+        batch_points_time = torch.cat([batch_points, batch_time.expand(batch_points[..., :1].shape)], dim=-1)
+        batch_points_time_flat = batch_points_time.reshape(-1, 4)
+
+        bbox_mask = insideMask(batch_points_time_flat[..., :3], self.s_w2s, self.s_scale, self.s_min, self.s_max, to_float=False)
+        batch_points_time_flat_filtered = batch_points_time_flat[bbox_mask]
+        batch_points_time_flat_filtered.requires_grad = True
+
+        hidden = self.encoder_d(batch_points_time_flat_filtered)
+        raw_d = self.model_d(hidden)
+
+        jac = torch.vmap(torch.func.jacrev(g))(hidden)
+        jac_x = get_minibatch_jacobian(hidden, batch_points_time_flat_filtered)
+        jac = jac @ jac_x
+        _d_x, _d_y, _d_z, _d_t = [torch.squeeze(_, -1) for _ in jac.split(1, dim=-1)]
+
+        raw_vel, raw_f = self.model_v(self.encoder_v(batch_points_time_flat_filtered))
+        _u_x, _u_y, _u_z, _u_t = None, None, None, None
+        _u, _v, _w = raw_vel.split(1, dim=-1)
+        split_nse = _d_t + (_u * _d_x + _v * _d_y + _w * _d_z)
+        nse_errors = torch.mean(torch.square(split_nse))
+        split_nse_wei = 0.001
+        nseloss_fine = nse_errors * split_nse_wei
+
+        proj_loss = torch.zeros_like(nseloss_fine)
+
+        viz_dens_mask = raw_d.detach() > 0.1
+        vel_norm = raw_vel.norm(dim=-1, keepdim=True)
+        min_vel_mask = vel_norm.detach() < 0.2 * raw_d.detach()
+        vel_reg_mask = min_vel_mask & viz_dens_mask
+        min_vel_reg_map = (0.2 * raw_d - vel_norm) * vel_reg_mask.float()
+        min_vel_reg = min_vel_reg_map.pow(2).mean()
+        # min_vel_reg = torch.zeros_like(nseloss_fine)
+
+        skip = False
+        if nse_errors.sum() > 10000:
+            print(f'skip large loss {nse_errors.sum():.3g}, timestep={batch_points_time_flat_filtered[0, 3]}')
+            skip = True
+        return skip, nseloss_fine, proj_loss, min_vel_reg
 
     def joint_loss(self, batch_indices, batch_rays_o, batch_rays_d, depth_size: int, near: float, far: float):
         def g(x):
@@ -278,7 +354,7 @@ class ValidationModel:
     @torch.compile
     def sample_density_grid(self, resx, resy, resz, frame):
         with torch.no_grad():
-            xs, ys, zs = torch.meshgrid([torch.linspace(0, 1, resx, device=self.target_device), torch.linspace(0, 1, resy, device=self.target_device), torch.linspace(0, 1, resz, device=self.target_device)], indexing='ij')
+            xs, ys, zs = torch.meshgrid([torch.linspace(0, 1, resx, device=self.target_device, dtype=self.target_dtype), torch.linspace(0, 1, resy, device=self.target_device, dtype=self.target_dtype), torch.linspace(0, 1, resz, device=self.target_device, dtype=self.target_dtype)], indexing='ij')
             coord_3d_sim = torch.stack([xs, ys, zs], dim=-1)
             coord_3d_world = sim2world(coord_3d_sim, self.s2w, self.s_scale)
             input_xyzt_flat = torch.cat([coord_3d_world, torch.ones_like(coord_3d_world[..., :1]) * float(frame / 120.0)], dim=-1).reshape(-1, 4)
@@ -298,7 +374,7 @@ class ValidationModel:
     @torch.compile
     def sample_velocity_grid(self, resx, resy, resz, frame):
         with torch.no_grad():
-            xs, ys, zs = torch.meshgrid([torch.linspace(0, 1, resx, device=self.target_device), torch.linspace(0, 1, resy, device=self.target_device), torch.linspace(0, 1, resz, device=self.target_device)], indexing='ij')
+            xs, ys, zs = torch.meshgrid([torch.linspace(0, 1, resx, device=self.target_device, dtype=self.target_dtype), torch.linspace(0, 1, resy, device=self.target_device, dtype=self.target_dtype), torch.linspace(0, 1, resz, device=self.target_device, dtype=self.target_dtype)], indexing='ij')
             coord_3d_sim = torch.stack([xs, ys, zs], dim=-1)
             coord_3d_world = sim2world(coord_3d_sim, self.s2w, self.s_scale)
             input_xyzt_flat = torch.cat([coord_3d_world, torch.ones_like(coord_3d_world[..., :1]) * float(frame / 120.0)], dim=-1).reshape(-1, 4)
@@ -331,11 +407,10 @@ def train_density_only(total_iter, batch_size, depth_size, ratio, target_device,
             losses.append(img_loss.item())
             steps.append(model.global_step)
 
-            if len(losses) % 100 == 0:
+            if len(steps) % 100 == 0:
                 avg_loss = sum(losses[-100:]) / 100
                 avg_losses.append(avg_loss)
                 avg_steps.append(model.global_step)
-
     except Exception as e:
         print(e)
     finally:
@@ -364,6 +439,84 @@ def train_density_only(total_iter, batch_size, depth_size, ratio, target_device,
         print(f"Image loss curve saved to {save_path}")
 
 
+def train_velocity_only(total_iter, batch_size, ratio, target_device, target_dtype, pretrained_ckpt=None):
+    losses_nseloss_fine = []
+    losses_proj = []
+    losses_min_vel_reg = []
+    steps = []
+    avg_losses_nseloss_fine = []
+    avg_losses_proj = []
+    avg_losses_min_vel_reg = []
+    avg_steps = []
+
+    model = TrainModel(batch_size, ratio, target_device, target_dtype)
+    if pretrained_ckpt:
+        model.load_ckpt(pretrained_ckpt)
+    try:
+        points_generator = sample_bbox(128, 192, 128, 1024, True, target_device, target_dtype, model.s2w, model.s_w2s, model.s_scale, model.s_min, model.s_max)
+        for _ in tqdm.trange(total_iter):
+            try:
+                batch_points = next(points_generator)
+            except StopIteration:
+                points_generator = sample_bbox(128, 192, 128, 1024, True, target_device, target_dtype, model.s2w, model.s_w2s, model.s_scale, model.s_min, model.s_max)
+                batch_points = next(points_generator)
+            nseloss_fine, proj_loss, min_vel_reg = model.optimize_velocity(batch_points)
+            losses_nseloss_fine.append(nseloss_fine.item())
+            losses_proj.append(proj_loss.item())
+            losses_min_vel_reg.append(min_vel_reg.item())
+            steps.append(model.global_step)
+
+            if len(steps) % 100 == 0:
+                avg_loss_nseloss_fine = sum(losses_nseloss_fine[-100:]) / 100
+                avg_loss_proj = sum(losses_proj[-100:]) / 100
+                avg_loss_min_vel_reg = sum(losses_min_vel_reg[-100:]) / 100
+                avg_losses_nseloss_fine.append(avg_loss_nseloss_fine)
+                avg_losses_proj.append(avg_loss_proj)
+                avg_losses_min_vel_reg.append(avg_loss_min_vel_reg)
+                avg_steps.append(model.global_step)
+    except Exception as e:
+        print(e)
+    finally:
+        model.save_ckpt('ckpt/train_velocity_only')
+        from datetime import datetime
+        import os
+        import matplotlib.pyplot as plt
+
+        # 获取时间戳和保存目录
+        timestamp = datetime.now().strftime("%m%d%H")
+        save_dir = 'ckpt/image'
+        os.makedirs(save_dir, exist_ok=True)
+
+        # 定义四个损失及其属性
+        loss_data = [
+            ("nseloss_fine", avg_steps, avg_losses_nseloss_fine, 'red', 'Average NSE Loss Fine (every 100 steps)'),
+            ("proj_loss", avg_steps, avg_losses_proj, 'green', 'Average Proj Loss (every 100 steps)'),
+            ("min_vel_reg", avg_steps, avg_losses_min_vel_reg, 'purple', 'Average Min Vel Reg Loss (every 100 steps)')
+        ]
+
+        # 绘制并保存四个独立的损失曲线
+        for loss_name, steps, losses, color, title in loss_data:
+            # 构建文件名
+            filename = f"loss_{loss_name}_{timestamp}_bs{batch_size}_{model.global_step}.png"
+            save_path = os.path.join(save_dir, filename)
+
+            # 绘制曲线
+            plt.figure(figsize=(8, 6))
+            plt.plot(steps, losses, label=title, color=color, linestyle='-', marker='o')
+            plt.xlabel('Global Step')
+            plt.ylabel('Loss (Averaged)')
+            plt.title(f'{title} vs. Global Step')
+            plt.legend()
+            plt.grid(True)
+            plt.show()
+
+            # 保存图片
+            plt.savefig(save_path)
+            plt.close()
+
+            print(f"{loss_name} loss curve saved to {save_path}")
+
+
 def train_joint(total_iter, batch_size, depth_size, ratio, target_device, target_dtype, pretrained_ckpt=None):
     losses_nseloss_fine = []
     losses_img = []
@@ -388,7 +541,7 @@ def train_joint(total_iter, batch_size, depth_size, ratio, target_device, target
             losses_min_vel_reg.append(min_vel_reg.item())
             steps.append(model.global_step)
 
-            if len(losses_img) % 100 == 0:
+            if len(steps) % 100 == 0:
                 avg_loss_nseloss_fine = sum(losses_nseloss_fine[-100:]) / 100
                 avg_loss_img = sum(losses_img[-100:]) / 100
                 avg_loss_proj = sum(losses_proj[-100:]) / 100
@@ -398,11 +551,10 @@ def train_joint(total_iter, batch_size, depth_size, ratio, target_device, target
                 avg_losses_proj.append(avg_loss_proj)
                 avg_losses_min_vel_reg.append(avg_loss_min_vel_reg)
                 avg_steps.append(model.global_step)
-
     except Exception as e:
         print(e)
     finally:
-        model.save_ckpt('ckpt/train_density_only')
+        model.save_ckpt('ckpt/train_joint')
         from datetime import datetime
         import os
         import matplotlib.pyplot as plt
@@ -463,7 +615,17 @@ if __name__ == "__main__":
             ratio=0.5,
             target_device=torch.device("cuda:0"),
             target_dtype=torch.float32,
-            pretrained_ckpt=None
+            pretrained_ckpt=None,
+        )
+
+    if option == "train_velocity_only":
+        train_velocity_only(
+            total_iter=1000,
+            batch_size=1024,
+            ratio=0.5,
+            target_device=torch.device("cuda:0"),
+            target_dtype=torch.float32,
+            pretrained_ckpt='ckpt/train_density_only/ckpt_033120_bs1024_100000.tar'
         )
 
     if option == "train_joint":
@@ -474,7 +636,7 @@ if __name__ == "__main__":
             ratio=0.5,
             target_device=torch.device("cuda:0"),
             target_dtype=torch.float32,
-            pretrained_ckpt=None
+            pretrained_ckpt=None,
         )
 
     if option == "validate_sample_grid":
