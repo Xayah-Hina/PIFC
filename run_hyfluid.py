390e0ed52b026affe813523535e9270399af1b19
Xayah <ycxiang14sep@gmail.com>
2025-04-14 16:16:20 +0900

    add tensorboard

--- run_hyfluid.py
+++ run_hyfluid.py
@@ -2,52 +2,67 @@ from train import *
 from evaluate import *
 
 
-def train_density_only(config: TrainConfig, total_iter, final=False, pretrained_ckpt=None):
-    losses = []
-    steps = []
-    avg_losses = []
-    avg_steps = []
+def get_current_function_name():
+    import inspect
+    return inspect.currentframe().f_back.f_code.co_name
 
+
+def train_density_only(config: TrainConfig, total_iter: int, pretrained_ckpt=None):
     model = TrainDensityModel(config)
-    if pretrained_ckpt:
-        model.load_ckpt(pretrained_ckpt, config.target_device)
-    import tqdm
+    from torch.utils.tensorboard import SummaryWriter
+    from datetime import datetime
+    writer = SummaryWriter(log_dir=f"ckpt/{get_current_function_name()}/{datetime.now().strftime('%m%d%H')}")
     try:
+        if pretrained_ckpt:
+            model.load_ckpt(pretrained_ckpt, config.target_device)
+        import tqdm
         for _ in tqdm.trange(total_iter):
             img_loss = model.forward(config.batch_size, config.depth_size)
-            losses.append(img_loss.item())
-            steps.append(model.global_step)
-
+            writer.add_scalar("Loss/train", img_loss, _)
             tqdm.tqdm.write(f"iter: {model.global_step}, lr_d: {model.scheduler_d.get_last_lr()[0]}, img_loss: {img_loss}")
-            if len(steps) % 100 == 0:
-                avg_loss = sum(losses[-100:]) / 100
-                avg_losses.append(avg_loss)
-                avg_steps.append(model.global_step)
     except Exception as e:
         print(e)
     finally:
-        model.save_ckpt(f'ckpt/{config.scene_name}/train_density_only', final=final)
+        model.save_ckpt(f'ckpt/{config.scene_name}/{get_current_function_name()}', final=False)
+        writer.close()
 
-        from datetime import datetime
-        timestamp = datetime.now().strftime("%m%d%H")
-        filename = f"loss_train_density_only_{timestamp}_bs{config.batch_size}_{model.global_step}.png"
-        save_dir = f'ckpt/{config.scene_name}/image'
-        os.makedirs(save_dir, exist_ok=True)
-        save_path = os.path.join(save_dir, filename)
 
-        import matplotlib.pyplot as plt
-        plt.figure(figsize=(8, 6))
-        plt.plot(avg_steps, avg_losses, label='Average Image Loss (every 100 steps)', color='blue', linestyle='-', marker='o')
-        plt.xlabel('Global Step')
-        plt.ylabel('Image Loss (Averaged)')
-        plt.title('Average Image Loss vs. Global Step')
-        plt.legend()
-        plt.grid(True)
-        plt.savefig(save_path)
-        plt.show()
-        plt.close()
+def train_velocity(config: TrainConfig, total_iter: int, pretrained_ckpt=None):
+    model = TrainVelocityModel(config)
+    from torch.utils.tensorboard import SummaryWriter
+    from datetime import datetime
+    writer = SummaryWriter(log_dir=f"ckpt/{get_current_function_name()}/{datetime.now().strftime('%m%d%H')}")
+    try:
+        if pretrained_ckpt:
+            model.load_ckpt(pretrained_ckpt, config.target_device)
+        import tqdm
+        for _ in tqdm.trange(total_iter):
+            vel_loss, nseloss_fine, proj_loss, min_vel_reg = model.forward(...)
+            tqdm.tqdm.write(f"iter: {model.global_step}, lr_d: {model.scheduler_d.get_last_lr()[0]}, lr_v: {model.scheduler_v.get_last_lr()[0]}, vel_loss: {vel_loss}, nseloss_fine: {nseloss_fine}, proj_loss: {proj_loss}, min_vel_reg: {min_vel_reg}")
+    except Exception as e:
+        print(e)
+    finally:
+        model.save_ckpt(f'ckpt/{config.scene_name}/{get_current_function_name()}', final=False)
+        writer.close()
 
-        print(f"Image loss curve saved to {save_path}")
+
+def train_joint(config: TrainConfig, total_iter: int, pretrained_ckpt=None):
+    model = TrainJointModel(config)
+    from torch.utils.tensorboard import SummaryWriter
+    from datetime import datetime
+    writer = SummaryWriter(log_dir=f"ckpt/{get_current_function_name()}/{datetime.now().strftime('%m%d%H')}")
+    try:
+        if pretrained_ckpt:
+            model.load_ckpt(pretrained_ckpt, config.target_device)
+        import tqdm
+        for _ in tqdm.trange(total_iter):
+            vel_loss, nseloss_fine, img_loss, proj_loss, min_vel_reg = model.forward(config.batch_size, config.depth_size)
+            tqdm.tqdm.write(f"iter: {model.global_step}, lr_d: {model.scheduler_d.get_last_lr()[0]}, lr_v: {model.scheduler_v.get_last_lr()[0]}, vel_loss: {vel_loss}, nseloss_fine: {nseloss_fine}, img_loss: {img_loss}, proj_loss: {proj_loss}, min_vel_reg: {min_vel_reg}")
+    except Exception as e:
+        print(e)
+    finally:
+        model.save_ckpt(f'ckpt/{config.scene_name}/{get_current_function_name()}', final=False)
+        writer.close()
 
 
 def evaluate_render_frame(config: EvaluationConfig, frame, pose, focal, width, height, depth_size, near, far):
@@ -77,7 +92,7 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="Run training or validation.")
-    parser.add_argument('--option', type=str, choices=['train_density_only', 'train_velocity_only', 'train_joint', 'evaluate_render_frame'], required=True, help="Choose the operation to execute.")
+    parser.add_argument('--option', type=str, choices=['train_density_only', 'train_velocity', 'train_joint', 'evaluate_render_frame'], required=True, help="Choose the operation to execute.")
     parser.add_argument('--device', type=str, default="cuda:0", help="Device to run the operation.")
     parser.add_argument('--dtype', type=str, default="float32", choices=['float32', 'float16'], help="Data type to use.")
     parser.add_argument('--scene', type=str, default="hyfluid", help="Scene to run.")
@@ -99,7 +114,6 @@ if __name__ == "__main__":
                 ratio=args.ratio,
             ),
             total_iter=args.total_iter,
-            final=False,
             pretrained_ckpt=args.checkpoint
         )
 
--- train.py
+++ train.py
@@ -40,7 +40,7 @@ class TrainConfig:
         self.s_scale = self.voxel_scale.expand([3])
 
 
-class TrainModelBase:
+class _TrainModelBase:
     def __init__(self, config: TrainConfig):
         self._reinitialize(config)
 
@@ -136,7 +136,7 @@ class TrainModelBase:
             print(f"Error loading model: {e}")
 
 
-class TrainDensityModel(TrainModelBase):
+class TrainDensityModel(_TrainModelBase):
     def __init__(self, config: TrainConfig):
         super().__init__(config)
 
@@ -192,3 +192,154 @@ class TrainDensityModel(TrainModelBase):
         self.scheduler_d.step()
         self.global_step += 1
         return img_loss
+
+
+class TrainVelocityModel(_TrainModelBase):
+    def __init__(self, config: TrainConfig):
+        super().__init__(config)
+
+    def velocity_loss(self, batch_points):
+        def g(x):
+            return self.model_d(x)
+
+        batch_time = torch.rand((), device=self.target_device, dtype=self.target_dtype)
+        batch_points_time = torch.cat([batch_points, batch_time.expand(batch_points[..., :1].shape)], dim=-1)
+        batch_points_time_flat = batch_points_time.reshape(-1, 4)
+
+        bbox_mask = insideMask(batch_points_time_flat[..., :3], self.s_w2s, self.s_scale, self.s_min, self.s_max, to_float=False)
+        batch_points_time_flat_filtered = batch_points_time_flat[bbox_mask]
+        batch_points_time_flat_filtered.requires_grad = True
+
+        hidden = self.encoder_d(batch_points_time_flat_filtered)
+        raw_d = self.model_d(hidden)
+
+        jac = torch.vmap(torch.func.jacrev(g))(hidden)
+        jac_x = get_minibatch_jacobian(hidden, batch_points_time_flat_filtered)
+        jac = jac @ jac_x
+        _d_x, _d_y, _d_z, _d_t = [torch.squeeze(_, -1) for _ in jac.split(1, dim=-1)]
+
+        raw_vel, raw_f = self.model_v(self.encoder_v(batch_points_time_flat_filtered))
+        _u_x, _u_y, _u_z, _u_t = None, None, None, None
+        _u, _v, _w = raw_vel.split(1, dim=-1)
+        split_nse = _d_t + (_u * _d_x + _v * _d_y + _w * _d_z)
+        nse_errors = torch.mean(torch.square(split_nse))
+        split_nse_wei = 0.001
+        nseloss_fine = nse_errors * split_nse_wei
+
+        proj_loss = torch.zeros_like(nseloss_fine)
+
+        viz_dens_mask = raw_d.detach() > 0.1
+        vel_norm = raw_vel.norm(dim=-1, keepdim=True)
+        min_vel_mask = vel_norm.detach() < 0.2 * raw_d.detach()
+        vel_reg_mask = min_vel_mask & viz_dens_mask
+        min_vel_reg_map = (0.2 * raw_d - vel_norm) * vel_reg_mask.float()
+        min_vel_reg = min_vel_reg_map.pow(2).mean()
+
+        skip = False
+        if nse_errors.sum() > 10000:
+            print(f'skip large loss {nse_errors.sum():.3g}, timestep={batch_points_time_flat_filtered[0, 3]}')
+            skip = True
+        return skip, nseloss_fine, proj_loss, min_vel_reg
+
+    def forward(self, sampled_points: torch.Tensor):
+        self.optimizer_v.zero_grad()
+        skip, nseloss_fine, proj_loss, min_vel_reg = self.velocity_loss(batch_points=sampled_points)
+        vel_loss = 1.0 * nseloss_fine + 1.0 * proj_loss + 10.0 * min_vel_reg
+        if not skip:
+            vel_loss.backward()
+            self.optimizer_v.step()
+            self.scheduler_v.step()
+            self.global_step += 1
+        return vel_loss, nseloss_fine, proj_loss, min_vel_reg
+
+
+class TrainJointModel(_TrainModelBase):
+    def __init__(self, config: TrainConfig):
+        super().__init__(config)
+
+    def joint_loss(self, batch_indices, batch_rays_o, batch_rays_d, depth_size: int, near: float, far: float):
+        def g(x):
+            return self.model_d(x)
+
+        batch_time, batch_target_pixels = sample_random_frame(videos_data=self.videos_data_resampled, batch_indices=batch_indices, device=self.target_device, dtype=self.target_dtype)
+        batch_size_current = batch_rays_d.shape[0]
+
+        t_vals = torch.linspace(0., 1., steps=depth_size, device=self.target_device, dtype=self.target_dtype)
+        t_vals = t_vals.view(1, depth_size)
+        z_vals = near * (1. - t_vals) + far * t_vals
+        z_vals = z_vals.expand(batch_size_current, depth_size)
+
+        mid_vals = .5 * (z_vals[..., 1:] + z_vals[..., :-1])
+        upper_vals = torch.cat([mid_vals, z_vals[..., -1:]], -1)
+        lower_vals = torch.cat([z_vals[..., :1], mid_vals], -1)
+        t_rand = torch.rand(z_vals.shape, device=self.target_device, dtype=self.target_dtype)
+        z_vals = lower_vals + (upper_vals - lower_vals) * t_rand
+
+        batch_dist_vals = z_vals[..., 1:] - z_vals[..., :-1]  # [batch_size_current, N_depths-1]
+        batch_points = batch_rays_o[:, None, :] + batch_rays_d[:, None, :] * z_vals[..., :, None]  # [batch_size_current, N_depths, 3]
+        batch_points_time = torch.cat([batch_points, batch_time.expand(batch_points[..., :1].shape)], dim=-1)
+        batch_points_time_flat = batch_points_time.reshape(-1, 4)
+
+        bbox_mask = insideMask(batch_points_time_flat[..., :3], self.s_w2s, self.s_scale, self.s_min, self.s_max, to_float=False)
+        batch_points_time_flat_filtered = batch_points_time_flat[bbox_mask]
+        batch_points_time_flat_filtered.requires_grad = True
+
+        hidden = self.encoder_d(batch_points_time_flat_filtered)
+        raw_d = self.model_d(hidden)
+        raw_flat = torch.zeros([batch_size_current * depth_size], device=self.target_device, dtype=self.target_dtype)
+        raw_d_flat = raw_d.view(-1)
+        raw_flat = raw_flat.masked_scatter(bbox_mask, raw_d_flat)
+        raw = raw_flat.reshape(batch_size_current, depth_size, 1)
+
+        dists_cat = torch.cat([batch_dist_vals, torch.tensor([1e10], device=self.target_device).expand(batch_dist_vals[..., :1].shape)], -1)  # [batch_size_current, N_depths]
+        dists_final = dists_cat * torch.norm(batch_rays_d[..., None, :], dim=-1)  # [batch_size_current, N_depths]
+
+        rgb_trained = torch.ones(3, device=self.target_device) * (0.6 + torch.tanh(self.model_d.rgb) * 0.4)
+        noise = 0.
+        alpha = 1. - torch.exp(-torch.nn.functional.relu(raw[..., -1] + noise) * dists_final)
+        weights = alpha * torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1), device=self.target_device), 1. - alpha + 1e-10], -1), -1)[:, :-1]
+        rgb_map = torch.sum(weights[..., None] * rgb_trained, -2)
+        img_loss = torch.nn.functional.mse_loss(rgb_map, batch_target_pixels)
+
+        jac = torch.vmap(torch.func.jacrev(g))(hidden)
+        jac_x = get_minibatch_jacobian(hidden, batch_points_time_flat_filtered)
+        jac = jac @ jac_x
+        _d_x, _d_y, _d_z, _d_t = [torch.squeeze(_, -1) for _ in jac.split(1, dim=-1)]
+
+        raw_vel, raw_f = self.model_v(self.encoder_v(batch_points_time_flat_filtered))
+        _u_x, _u_y, _u_z, _u_t = None, None, None, None
+        _u, _v, _w = raw_vel.split(1, dim=-1)
+        split_nse = _d_t + (_u * _d_x + _v * _d_y + _w * _d_z)
+        nse_errors = torch.mean(torch.square(split_nse))
+        split_nse_wei = 0.001
+        nseloss_fine = nse_errors * split_nse_wei
+
+        proj_loss = torch.zeros_like(nseloss_fine)
+
+        viz_dens_mask = raw_d.detach() > 0.1
+        vel_norm = raw_vel.norm(dim=-1, keepdim=True)
+        min_vel_mask = vel_norm.detach() < 0.2 * raw_d.detach()
+        vel_reg_mask = min_vel_mask & viz_dens_mask
+        min_vel_reg_map = (0.2 * raw_d - vel_norm) * vel_reg_mask.float()
+        min_vel_reg = min_vel_reg_map.pow(2).mean()
+
+        skip = False
+        if nse_errors.sum() > 10000:
+            print(f'skip large loss {nse_errors.sum():.3g}, timestep={batch_points_time_flat_filtered[0, 3]}')
+            skip = True
+        return skip, nseloss_fine, img_loss, proj_loss, min_vel_reg
+
+    def forward(self, batch_size: int, depth_size: int):
+        self.optimizer_d.zero_grad()
+        self.optimizer_v.zero_grad()
+        batch_indices, batch_rays_o, batch_rays_d = self._next_batch(batch_size)
+        skip, nseloss_fine, img_loss, proj_loss, min_vel_reg = self.joint_loss(batch_indices, batch_rays_o, batch_rays_d, depth_size, float(self.near[0].item()), float(self.far[0].item()))
+        vel_loss = 10000 * img_loss + 1.0 * nseloss_fine + 1.0 * proj_loss + 10.0 * min_vel_reg
+        if not skip:
+            vel_loss.backward()
+            self.optimizer_d.step()
+            self.optimizer_v.step()
+            self.scheduler_d.step()
+            self.scheduler_v.step()
+            self.global_step += 1
+        return vel_loss, nseloss_fine, img_loss, proj_loss, min_vel_reg
