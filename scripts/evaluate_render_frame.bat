6762cdb730d24719ee4bc0500101a5a2966499ea
Xayah <ycxiang14sep@gmail.com>
2025-04-14 16:56:03 +0900

    update scripts

---
+++ scripts/evaluate_render_frame.bat
@@ -0,0 +1,17 @@
+@echo off
+set "CKPT=%~1"
+
+REM 切换到当前 .bat 文件所在的目录
+cd /d %~dp0
+
+REM 进入上一级目录
+cd ..
+
+REM 固定参数 --option
+set "OPTION=evaluate_render_frame"
+
+REM 调用 Python 脚本
+"C:/Program Files/Side Effects Software/Houdini 20.5.550/python311/python.exe" run_hyfluid.py --option=%OPTION% --checkpoint="%CKPT%"
+
+REM 可选：运行完暂停窗口
+pause
