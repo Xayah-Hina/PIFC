a832c5fc9a9a9acf19557c76d75aeb146d0a8874
Xayah <ycxiang14sep@gmail.com>
2025-04-19 17:51:54 +0900

    default dir to history

--- run_evaluation.py
+++ run_evaluation.py
@@ -10,7 +10,7 @@ def open_file_dialog():
         root = Tk()
         root.withdraw()
         root.attributes("-topmost", True)
-        file_path = filedialog.askopenfilename(title="Select a checkpoint file", initialdir="ckpt", filetypes=[("Checkpoint files", "*.tar")])
+        file_path = filedialog.askopenfilename(title="Select a checkpoint file", initialdir="history", filetypes=[("Checkpoint files", "*.tar")])
         root.destroy()
 
         if file_path and os.path.isfile(file_path):
