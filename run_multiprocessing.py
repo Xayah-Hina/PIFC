c75f100c9edf1ab4b33e41feb80e6604e3a51ec3
Xayah <ycxiang14sep@gmail.com>
2025-04-21 20:45:01 +0900

    add multi- resimulation

--- run_multiprocessing.py
+++ run_multiprocessing.py
@@ -1,5 +1,6 @@
 import subprocess
 
+HYTHON_EXE = "C:/Program Files/Side Effects Software/Houdini 20.5.550/bin/hython.exe"
 
 def train_velocity_multiprocessing(scene, devices):
     tags = ["TAG1", "TAG2", "TAG3", "TAG4", "TAG5", "TAG6", "TAG7", "TAG8", "TAG9", "TAG10"]
@@ -19,7 +20,7 @@ def train_velocity_multiprocessing(scene, devices):
     device_iter = 0
     for tag, lw_img, lw_nse, lw_proj, lw_min_vel_reg, lw_lcc in zip(tags, lw_imgs, lw_nses, lw_projs, lw_min_vel_regs, lw_lccs):
         print(f"Training {tag}")
-        p = subprocess.Popen(["C:/Program Files/Side Effects Software/Houdini 20.5.550/bin/hython.exe", "run_train.py",
+        p = subprocess.Popen([HYTHON_EXE, "run_train.py",
                               f"--tag={tag}",
                               f"--option={option}",
                               f"--scene={scene}",
@@ -60,7 +61,7 @@ def train_velocity_lcc_multiprocessing(scene, devices):
     device_iter = 0
     for tag, lw_img, lw_nse, lw_proj, lw_min_vel_reg, lw_lcc in zip(tags, lw_imgs, lw_nses, lw_projs, lw_min_vel_regs, lw_lccs):
         print(f"Training {tag}")
-        p = subprocess.Popen(["C:/Program Files/Side Effects Software/Houdini 20.5.550/bin/hython.exe", "run_train.py",
+        p = subprocess.Popen([HYTHON_EXE, "run_train.py",
                               f"--tag={tag}",
                               f"--option={option}",
                               f"--scene={scene}",
@@ -95,7 +96,32 @@ def export_velocity_field_multiprocessing(pretrained_ckpt_path, devices):
     device_iter = 0
 
     for checkpoint in ckpt_files:
-        p = subprocess.Popen(["C:/Program Files/Side Effects Software/Houdini 20.5.550/bin/hython.exe", "run_evaluation.py",
+        p = subprocess.Popen([HYTHON_EXE, "run_evaluation.py",
+                              f"--option={option}",
+                              f"--checkpoint={checkpoint}",
+                              f"--device={devices[device_iter % device_num]}"])
+        device_iter += 1
+        processes.append(p)
+        import time
+        time.sleep(2.0)
+
+    for p in processes:
+        p.wait()
+
+
+def evaluate_resimulation_multiprocessing(pretrained_ckpt_path, devices):
+    option = "evaluate_resimulation"
+
+    from pathlib import Path
+    ckpt_files = list(Path(pretrained_ckpt_path).glob("*.tar"))
+    assert len(ckpt_files) > 0, f"Checkpoint files not found in {pretrained_ckpt_path}"
+
+    processes = []
+    device_num = len(devices)
+    device_iter = 0
+
+    for checkpoint in ckpt_files:
+        p = subprocess.Popen([HYTHON_EXE, "run_evaluation.py",
                               f"--option={option}",
                               f"--checkpoint={checkpoint}",
                               f"--device={devices[device_iter % device_num]}"])
@@ -109,6 +135,7 @@ def export_velocity_field_multiprocessing(pretrained_ckpt_path, devices):
 
 
 if __name__ == "__main__":
+    DEVICES = ["cuda:0", "cuda:1"]
     import argparse
 
     parser = argparse.ArgumentParser(description="Run multiple training scripts.")
@@ -117,12 +144,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.option == "train_velocity_multiprocessing":
-        train_velocity_multiprocessing(scene=args.scene, devices=["cuda:0", "cuda:1"])
-        export_velocity_field_multiprocessing(pretrained_ckpt_path=f"ckpt/{args.scene}/train_velocity", devices=["cuda:0", "cuda:1"])
+        train_velocity_multiprocessing(scene=args.scene, devices=DEVICES)
+        export_velocity_field_multiprocessing(pretrained_ckpt_path=f"ckpt/{args.scene}/train_velocity", devices=DEVICES)
+        evaluate_resimulation_multiprocessing(pretrained_ckpt_path=f"ckpt/{args.scene}/train_velocity", devices=DEVICES)
 
     if args.option == "train_velocity_lcc_multiprocessing":
-        train_velocity_lcc_multiprocessing(scene=args.scene, devices=["cuda:0", "cuda:1"])
-        export_velocity_field_multiprocessing(pretrained_ckpt_path=f"ckpt/{args.scene}/train_velocity_lcc", devices=["cuda:0", "cuda:1"])
+        train_velocity_lcc_multiprocessing(scene=args.scene, devices=DEVICES)
+        export_velocity_field_multiprocessing(pretrained_ckpt_path=f"ckpt/{args.scene}/train_velocity_lcc", devices=DEVICES)
+        evaluate_resimulation_multiprocessing(pretrained_ckpt_path=f"ckpt/{args.scene}/train_velocity_lcc", devices=DEVICES)
 
     if args.option == "evaluate_velocity_multiprocessing":
-        export_velocity_field_multiprocessing(pretrained_ckpt_path=f"ckpt/{args.scene}/train_velocity", devices=["cuda:0", "cuda:1"])
+        export_velocity_field_multiprocessing(pretrained_ckpt_path=f"ckpt/{args.scene}/train_velocity", devices=DEVICES)
